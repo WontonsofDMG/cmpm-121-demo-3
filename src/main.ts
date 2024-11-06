bc4959151efc91590c0cc15674e0303bb27b65ea
Mitchell Tai <mitchelltai221@gmail.com>
2024-11-06 12:39:17 -0800

    Added a button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 // todo
+// src/main.ts
+
+// Function to create a button and append it to the body
+function createButton() {
+  const button = document.createElement("button");
+  button.textContent = "Click me!";
+  button.onclick = () => alert("You clicked the button!");
+  document.body.appendChild(button);
+}
+
+// Call the function to create the button
+createButton();
